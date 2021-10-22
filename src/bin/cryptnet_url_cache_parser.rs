db8fd444a28fad7c47fac623b430342735a1f214
AbdulRhman Alfaifi <a.alfaifi.14@gmail.com>
2021-10-22 22:14:27 +0300

    Added Field to the parsed results
    * Parsed and added the field `LastModificationTimeHeader` to the output
    * Modified the name of the field `MetadataHash` to `E-Tag`
    * Simple modification

--- src/bin/cryptnet_url_cache_parser.rs
+++ src/bin/cryptnet_url_cache_parser.rs
@@ -1,12 +1,12 @@
-use cryptnet_url_cache_parser::CryptnetURLCacheParser;
 use clap::{App, Arg};
-use std::io::{self, Write};
-use std::fs::File;
+use cryptnet_url_cache_parser::CryptnetURLCacheParser;
 use glob::glob;
+use std::fs::File;
+use std::io::{self, Write};
 
 enum OutputFormat {
     JSONL,
-    CSV
+    CSV,
 }
 
 impl OutputFormat {
@@ -14,13 +14,12 @@ impl OutputFormat {
         match s {
             "jsonl" => OutputFormat::JSONL,
             "csv" => OutputFormat::CSV,
-            _ => OutputFormat::CSV
+            _ => OutputFormat::CSV,
         }
     }
 }
 
-fn parse_cli_args() -> clap::ArgMatches<'static>
-{
+fn parse_cli_args() -> clap::ArgMatches<'static> {
     App::new("CryptnetURLCacheParser")
         .version(env!("CARGO_PKG_VERSION"))
         .author("AbdulRhman Alfaifi - @A__ALFAIFI")
@@ -58,43 +57,60 @@ fn parse_cli_args() -> clap::ArgMatches<'static>
         .get_matches()
 }
 
-
-fn output_data_csv(parsed_data: &CryptnetURLCacheParser) -> io::Result<String>
-{
-    if parsed_data.sha256.is_none(){
-        return Ok(format!("\"{}\",\"{}\",\"{}\",\"{}\",\"{}\"\r\n",parsed_data.timestamp,parsed_data.url,parsed_data.file_size,parsed_data.metadata_hash,parsed_data.full_path))
-    }
-    else{
-        return Ok(format!("\"{}\",\"{}\",\"{}\",\"{}\",\"{}\",\"{}\"\r\n",parsed_data.timestamp,parsed_data.url,parsed_data.file_size,parsed_data.metadata_hash,parsed_data.full_path,parsed_data.sha256.as_ref().unwrap()))
+fn output_data_csv(parsed_data: &CryptnetURLCacheParser) -> io::Result<String> {
+    if parsed_data.sha256.is_none() {
+        return Ok(format!(
+            "\"{}\",\"{}\",\"{}\",\"{}\",\"{}\",\"{}\"\r\n",
+            parsed_data.last_download_time,
+            parsed_data.last_modification_time_header,
+            parsed_data.url,
+            parsed_data.file_size,
+            parsed_data.e_tag,
+            parsed_data.full_path
+        ));
+    } else {
+        return Ok(format!(
+            "\"{}\",\"{}\",\"{}\",\"{}\",\"{}\",\"{}\",\"{}\"\r\n",
+            parsed_data.last_download_time,
+            parsed_data.last_modification_time_header,
+            parsed_data.url,
+            parsed_data.file_size,
+            parsed_data.e_tag,
+            parsed_data.full_path,
+            parsed_data.sha256.as_ref().unwrap()
+        ));
     }
 }
 
-fn main()
-{
+fn main() {
     let args = parse_cli_args();
     let output_format = OutputFormat::from_str(args.value_of("output-format").unwrap());
     let output_to = args.value_of("output").unwrap();
-    let use_content = match args.occurrences_of("use-content"){
+    let use_content = match args.occurrences_of("use-content") {
         0 => Some(false),
-        _ => Some(true)
+        _ => Some(true),
     };
     let mut output: Box<dyn Write> = match output_to {
         "stdout" => Box::new(io::stdout()),
-        _ => Box::new(File::create(output_to).unwrap())
+        _ => Box::new(File::create(output_to).unwrap()),
     };
 
-    if args.occurrences_of("no-headers") == 0{
+    if args.occurrences_of("no-headers") == 0 {
         match output_format {
             OutputFormat::CSV => {
-                if use_content == Some(true){
-                    output.write("\"Timestamp\",\"URL\",\"FileSize (Bytes)\",\"MetadataHash\",\"FullPath\",\"SHA256\"\r\n".as_bytes()).expect("Error Writing Data !");
-                }
-                else{
-                    output.write("\"Timestamp\",\"URL\",\"FileSize (Bytes)\",\"MetadataHash\",\"FullPath\"\r\n".as_bytes()).expect("Error Writing Data !");
+                if use_content == Some(true) {
+                    output.write("\"LastDownloadTime\",\"LastModificationTimeHeader\",\"URL\",\"FileSize (Bytes)\",\"ETag\",\"FullPath\",\"SHA256\"\r\n".as_bytes()).expect("Error Writing Data !");
+                } else {
+                    output
+                        .write(
+                            "\"LastDownloadTime\",\"LastModificationTimeHeader\",\"URL\",\"FileSize (Bytes)\",\"ETag\",\"FullPath\"\r\n"
+                                .as_bytes(),
+                        )
+                        .expect("Error Writing Data !");
                 }
                 ()
-            },
-            _ => ()
+            }
+            _ => (),
         };
     }
     // Default windows paths for CryptnetURLCache metadata/content files
@@ -112,20 +128,21 @@ fn main()
             match entry {
                 Ok(path) => {
                     let fullpath = path.as_path().to_str().unwrap();
-                    let parsed = CryptnetURLCacheParser::parse_file(fullpath,use_content).unwrap();
-                    match output_format 
-                    {
+                    let parsed = CryptnetURLCacheParser::parse_file(fullpath, use_content).unwrap();
+                    match output_format {
                         OutputFormat::JSONL => {
                             let json_data = serde_json::to_string(&parsed).unwrap();
-                            output.write(json_data.as_bytes()).expect("Error Writing Data !");
+                            output
+                                .write(json_data.as_bytes())
+                                .expect("Error Writing Data !");
                             output.write(b"\r\n").expect("Error Writing Data !");
-                        },
+                        }
                         OutputFormat::CSV => {
                             let data = output_data_csv(&parsed).unwrap();
                             output.write(data.as_bytes()).expect("Error Writing Data !");
                         }
                     }
-                },
+                }
                 Err(e) => println!("{:?}", e),
             }
         }
